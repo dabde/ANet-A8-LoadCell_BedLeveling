e0225746c77846bbdc4474f1e4d96c1a5a148206
DenisD <denis.dabischa@roddia.org>
2021-03-09 17:34:54 +0100

    Change to use movingAVG (no tare needed, only bump sensivity (2000) is important) Increase Serial Speed to 57600, to get more datapoints.

--- arduino/LoadCell_Switch/LoadCell_Switch.ino
+++ arduino/LoadCell_Switch/LoadCell_Switch.ino
@@ -1,33 +1,40 @@
 #include <HX711_ADC.h>
+#include <movingAvg.h>
 
 HX711_ADC LoadCell(2, 3);
+movingAvg mySensor(10);
 
-#define SAMPLES 4
-#define ANETA8_OUT 4
+#define ANETA8_OUT 5
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ANETA8_OUT, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(57600);
   Serial.println("Wait...");
   LoadCell.begin(64);
   long stabilisingtime = 25; // tare preciscion can be improved by adding a few seconds of stabilising time
   LoadCell.start(stabilisingtime);
   LoadCell.setCalFactor(696.0); // user set calibration factor (float)
+  mySensor.begin();
   Serial.println("Startup + tare is complete");
 }
 
 void loop() {
-  //update() should be called at least as often as HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS
-  //longer delay in scetch will reduce effective sample rate (be carefull with delay() in loop)
   LoadCell.update();
-  float i = LoadCell.getData();
-  if (i<-20) {
+  int LoadCellRead = round(LoadCell.getData()*1000);
+  mySensor.reading(LoadCellRead);
+  
+  if ((LoadCellRead-mySensor.getAvg())<-2000) {
     digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(ANETA8_OUT, LOW);
   } else {
     digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(ANETA8_OUT, HIGH);
   }
-  Serial.println(i);
+  
+  Serial.print(LoadCellRead);
+  Serial.print("\t");
+  Serial.print(mySensor.getAvg());
+  Serial.print("\t");
+  Serial.println(LoadCellRead-mySensor.getAvg());
 }
